3f8ae97a3a5a83e7b89c0f0f7de71d38cd0327c9
Cláudio Henriques <c.henriques@ua.pt>
2019-05-20 18:34:53 +0100

    Update all.py

--- megua/all.py
+++ megua/all.py
@@ -23,11 +23,11 @@ from megua.megoptions import MEGUA_PLATFORM
 if MEGUA_PLATFORM == "WINDOWS":
     print("here")
     import megua.mwindows as mwindows
-    ExLatex.print_instancte = mwindows.exlatex_print_instance
-    ExSiacua.print_instancte = mwindows.exsiacua_print_instance
+    ExLatex.print_instance = mwindows.exlatex_print_instance
+    ExSiacua.print_instance = mwindows.exsiacua_print_instance
     ExSiacua.conf__siacua_send = mwindows.conf__siacua_send
     ExSiacua.conf_siacuapreview = mwindows.conf_siacuapreview
-    ExAMC.print_instancte = mwindows.examc_print_instance
+    ExAMC.print_instance = mwindows.examc_print_instance
     MegBook.conf_catalog = mwindows.conf_catalog
     MegBook.conf_fast_exam_siacu = mwindows.conf_fast_exam_siacu
     MegBook.conf_latex_document = mwindows.conf_latex_document
@@ -37,11 +37,11 @@ if MEGUA_PLATFORM == "WINDOWS":
 
 elif MEGUA_PLATFORM == "SMC":
     import megua.msmc as msmc
-    ExLatex.print_instancte = msmc.exlatex_print_instance
-    ExSiacua.print_instancte = msmc.exsiacua_print_instance
+    ExLatex.print_instance = msmc.exlatex_print_instance
+    ExSiacua.print_instance = msmc.exsiacua_print_instance
     ExSiacua.conf__siacua_send = msmc.conf__siacua_send
     ExSiacua.conf_siacuapreview = msmc.conf_siacuapreview
-    ExAMC.print_instancte = msmc.examc_print_instance
+    ExAMC.print_instance = msmc.examc_print_instance
     MegBook.conf_catalog = msmc.conf_catalog
     MegBook.conf_fast_exam_siacu = msmc.conf_fast_exam_siacu
     MegBook.conf_latex_document = msmc.conf_latex_document
@@ -51,11 +51,11 @@ elif MEGUA_PLATFORM == "SMC":
 
 elif MEGUA_PLATFORM == "DESKTOP":
     import megua.mdesktop as mdesktop
-    ExLatex.print_instancte = mdesktop.exlatex_print_instance
-    ExSiacua.print_instancte = mdesktop.exsiacua_print_instance
+    ExLatex.print_instance = mdesktop.exlatex_print_instance
+    ExSiacua.print_instance = mdesktop.exsiacua_print_instance
     ExSiacua.conf__siacua_send = mdesktop.conf__siacua_send
     ExSiacua.conf_siacuapreview = mdesktop.conf_siacuapreview
-    ExAMC.print_instancte = mdesktop.examc_print_instance
+    ExAMC.print_instance = mdesktop.examc_print_instance
     MegBook.conf_catalog = mdesktop.conf_catalog
     MegBook.conf_fast_exam_siacu = mdesktop.conf_fast_exam_siacu
     MegBook.conf_latex_document = mdesktop.conf_latex_document
